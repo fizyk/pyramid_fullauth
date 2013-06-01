d0a8a51997c96a3de6290f7e39ffad41f290de8a
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2013-06-01 13:55:38 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=find_packages(),
