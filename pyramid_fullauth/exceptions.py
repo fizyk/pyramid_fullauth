2c21584ad12eebaac06873a085e27c7447921422
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2014-04-04 22:46:35 +0200

    exceptions and init

--- pyramid_fullauth/exceptions.py
+++ pyramid_fullauth/exceptions.py
@@ -1,54 +1,47 @@
-# -*- coding: utf-8 -*-
-
 # Copyright (c) 2013 - 2014 by pyramid_fullauth authors and contributors <see AUTHORS file>
 #
 # This module is part of pyramid_fullauth and is released under
 # the MIT License (MIT): http://opensource.org/licenses/MIT
+"""Fullauth's exceptions definition."""
 
 
 class DeleteException(Exception):
 
-    '''
-        Exception risen when the user can't be deleted
-    '''
+    """Exception risen when the user can't be deleted."""
+
     pass
 
 
 class ValidateError(ValueError):
 
-    '''
-        Base of every validate error in pyramid_fullauth
-    '''
+    """Base of every validate error in pyramid_fullauth."""
+
     pass
 
 
 class EmptyError(ValidateError):
 
-    '''
-        Thrown whenever user is trying to set empty value
-    '''
+    """Thrown whenever user is trying to set empty value."""
+
     pass
 
 
 class ShortPasswordError(ValidateError):
 
-    '''
-    Thrown when password doesn't meet the length requirement
-    '''
+    """Thrown when password doesn't meet the length requirement."""
+
     pass
 
 
 class PasswordConfirmMismatchError(ValidateError):
 
-    '''
-        Thrown when there's a mismatch with cpassword_confirm
-    '''
+    """Thrown when there's a mismatch with cpassword_confirm."""
+
     pass
 
 
 class EmailValidationError(ValidateError):
 
-    '''
-        Exception thrown, when there's incorrect email provided
-    '''
+    """Exception thrown, when there's incorrect email provided."""
+
     pass
