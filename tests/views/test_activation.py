188a9043969094ec472ede8bf4812f7062703bef
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2014-04-04 23:15:35 +0200

    xfail on proper condition

--- tests/views/test_activation.py
+++ tests/views/test_activation.py
@@ -45,11 +45,11 @@ def test_account_activation_wrong_key(user, db_session, default_app):
     assert not user.is_active
 
 
-def test_account_activation_key_with_trash_chars(request, user, db_session, default_app):
+def test_account_activation_key_with_trash_chars(user, db_session, default_app):
     '''
         Register:Activate user
     '''
-    if request.param == 'mysql':
+    if db_session.connection().dialect.name == 'mysql':
         pytest.xfail("failing due mysql default character set being latin1 and collation latin1_swedish_ci")
 
     user = db_session.merge(user)
