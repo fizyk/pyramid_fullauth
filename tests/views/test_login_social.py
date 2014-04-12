56ed7b03bb47a2a853fba836de0b499185a3761d
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2014-04-12 22:56:13 +0200

    test two social account connecion fails

--- tests/views/test_login_social.py
+++ tests/views/test_login_social.py
@@ -71,10 +71,9 @@ def test_login_different_social_account(social_config, db_session, facebook_user
     System should let user in, but not change connection.
     """
     # profile mock response
-    userid = u'2343'
     profile = {
         # facebook user id is different than user's
-        'accounts': [{'domain': u'facebook.com', 'userid': userid}],
+        'accounts': [{'domain': u'facebook.com', 'userid': u'2343'}],
         'displayName': u'teddy',
         'verifiedEmail': facebook_user.email,
         'preferredUsername': u'teddy',
@@ -97,7 +96,7 @@ def test_login_different_social_account(social_config, db_session, facebook_user
     out = view()
     # user should be authenticated recognized by email!
     assert out['status'] is True
-    assert facebook_user.provider_id('facebook') is not userid
+    assert facebook_user.provider_id('facebook') is not profile['accounts'][0]['userid']
 
 
 def test_login_social_connect(social_config, active_user, db_session):
@@ -161,3 +160,78 @@ def test_logged_social_connect_account(social_config, active_user, db_session):
     transaction.commit()
     user = db_session.merge(user)
     assert user.provider_id('facebook') == profile['accounts'][0]['userid']
+
+
+def test_logged_social_connect_second_account(social_config, facebook_user, db_session):
+    """Connect second facebook account to logged in user."""
+    user = db_session.merge(facebook_user)
+
+    # mock request
+    profile = {
+        'accounts': [{'domain': u'facebook.com', 'userid': u'2343'}],
+        'displayName': u'teddy',
+        'preferredUsername': u'teddy',
+        'emails': [{'value': u'aasd@basd.pl'}],
+        'name': u'ted'
+    }
+    credentials = {'oauthAccessToken': '7897048593434'}
+    provider_name = u'facebook'
+    provider_type = u'facebook'
+    request = testing.DummyRequest()
+    request.user = user
+    request.registry = social_config.registry
+    request.remote_addr = u'127.0.0.123'
+    request.context = AuthenticationComplete(profile, credentials, provider_name, provider_type)
+    request._ = lambda msg, *args, **kwargs: msg
+
+    request.login_perform = MagicMock(name='login_perform')
+    request.login_perform.return_value = {'status': True}
+    view = SocialLoginViews(request)
+    out = view()
+    # status should be false
+    assert out['status'] is False
+    assert out['msg'] == 'Your account is already connected to other ${provider} account.'
+    assert user.provider_id('facebook') is not profile['accounts'][0]['userid']
+
+
+def test_logged_social_connect_used_account(social_config, facebook_user, db_session):
+    """Connect second facebook account to logged in user."""
+    # this user will be logged and trying to connect facebook's user account.
+    fresh_user = User(
+        email='new@user.pl',
+        password='somepassword',
+        address_ip='127.0.0.1')
+    db_session.add(fresh_user)
+    transaction.commit()
+    user = db_session.merge(facebook_user)
+    fresh_user = db_session.merge(fresh_user)
+
+    # mock request
+    profile = {
+        'accounts': [{'domain': u'facebook.com', 'userid': user.provider_id('facebook')}],
+        'displayName': u'teddy',
+        'preferredUsername': u'teddy',
+        'emails': [{'value': u'aasd@basd.pl'}],
+        'name': u'ted'
+    }
+    credentials = {'oauthAccessToken': '7897048593434'}
+    provider_name = u'facebook'
+    provider_type = u'facebook'
+    request = testing.DummyRequest()
+    request.user = fresh_user
+    request.registry = social_config.registry
+    request.remote_addr = u'127.0.0.123'
+    request.context = AuthenticationComplete(profile, credentials, provider_name, provider_type)
+    request._ = lambda msg, *args, **kwargs: msg
+
+    request.login_perform = MagicMock(name='login_perform')
+    request.login_perform.return_value = {'status': True}
+    # call!
+    view = SocialLoginViews(request)
+    out = view()
+    # status should be false
+    assert out['status'] is False
+    assert out['msg'] == 'This ${provider} account is already connected with other account.'
+    transaction.begin()
+    fresh_user = db_session.merge(fresh_user)
+    assert fresh_user.provider_id('facebook') is None
