80bd3375034b424e19cee9579fe1a8e5aa4ab4ec
requires.io <support@requires.io>
2017-11-22 11:53:42 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ test_requires = [
     'pytest-pyramid==0.3.0',
     'pytest-cov==2.5.1',
     'pytest-mysql==1.1.1',
-    'pytest-postgresql==1.3.1',
+    'pytest-postgresql==1.3.2',
 ]
 
 extras_require = {
