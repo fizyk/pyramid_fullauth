b5f02a2097ccad1495a78ebc08fe4510d3b9ea83
requires.io <support@requires.io>
2018-06-20 13:54:14 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 test_requires = [
     'mock==2.0.0',
-    'pytest==3.6.1',
+    'pytest==3.6.2',
     'pytest-pyramid==0.3.0',
     'pytest-cov==2.5.1',
     'pytest-mysql==1.1.1',
